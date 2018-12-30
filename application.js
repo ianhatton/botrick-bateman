961f2a85f3a429117c31edd2d58c014475250235
Ian Hatton <mrianhatton@gmail.com>
2018-12-30 18:03:29 +0000

    Replies

--- application.js
+++ application.js
@@ -27,7 +27,7 @@ const snoowrap = new Snoowrap({
 const client = new Snoostorm(snoowrap),
       streamOptions = {
           subreddit: 'all',
-          results: 25
+          results: 10
       },
       comments = client.CommentStream(streamOptions),
       submissions = client.SubmissionStream(streamOptions);
@@ -36,8 +36,20 @@ const replies = {
     'aftershave': [
         'You should use an aftershave lotion with little or no alcohol. Never use cologne on your face, since the high alcohol content dries your face out and makes you look older. One should use an alcohol-free antibacterial toner with a water-moistened cotton ball to normalize the skin.'
     ],
+    'armani': [
+        'I pull my Armani shirt up and place her hand on my torso, wanting her to feel how rock-hard, how *halved* my stomach is, and I flex the muscles, grateful it’s light in the room so she can see how bronzed and defined my abdomen has become.'
+    ],
+    'butner': [
+        'Butner is wearing a pair of knee-length nylon and Lycra shorts with checkerboard inserts and a cotton and Lycra tank top and leather Reeboks.'
+    ],
+    'cheesecake': [
+        '“Cheesecake?” I say, confused by this plain, alien-sounding list. “What sauce or fruits were on the roasted chicken? What shapes was it cut into?”'
+    ],
     'cologne': [
         'You should use an aftershave lotion with little or no alcohol. Never use cologne on your face, since the high alcohol content dries your face out and makes you look older. One should use an alcohol-free antibacterial toner with a water-moistened cotton ball to normalize the skin.'
+    ],
+    'dorsia': [
+        'I stop looking through the Zagat guide and without glancing up, smiling tightly, stomach dropping, I silently ask myself, Do I really want to say no? Do I really want to say I can’t possibly get us in? Is that what I’m really prepared to do? Is that what I really want to do?'
     ],
     'gym': [
         'After getting dressed and putting my Walkman on, clipping its body to the Lycra shorts and placing the phones over my ears, a Stephen Bishop/Christopher Cross compilation tape Todd Hunter made for me, I check myself in the mirror before entering the gym and, dissatisfied, go back to my briefcase for some mousse to slick my hair back and then I use a moisturizer and, for a small blemish I notice under my lower lip, a dab of Clinique Touch-Stick. Satisfied, I turn the Walkman on, the volume up, and leave the locker room.',
@@ -47,18 +59,37 @@ const replies = {
         'No hardbodies at the gym today. Only faggots from the West Side, probably unemployed actors, waiters by night, and Muldwyn Butner of Sachs, who I went to Exeter with, over at the biceps curl machine.',
         'Two thousand abdominal crunches and thirty minutes of rope jumping in the living room, the Wurlitzer jukebox blasting “The Lion Sleeps Tonight” over and over, even though I worked out in the gym today for close to two hours.'
     ],
+    'huey lewis': [
+        'Huey Lewis and the News burst out of San Francisco onto the national music scene at the beginning of the decade, with their self-titled rock pop album released by Chrysalis, though they really didn’t come into their own, commercially or artistically, until their 1983 smash, Sports.'
+    ],
+    'lipstick': [
+        '*The Patty Winters Show* this morning was about Perfumes and Lipsticks and Makeups.'
+    ],
+    'makeup': [
+        '*The Patty Winters Show* this morning was about Perfumes and Lipsticks and Makeups.'
+    ],
     'mousse': [
         '“Hello, Halberstam,” Owen says, walking by.  “Hello, Owen,” I say, admiring the way he’s styled and slicked back his hair, with a part so even and sharp it... devastates me and I make a mental note to ask him where he purchases his hair-care products, which kind of mousse he uses, my final guess after mulling over the possibilities being Ten -X.'
     ],
     'pepsi': [
-        'I’m wandering around VideoVisions, the video rental store near my apartment on the Upper West Side, sipping from a can of Diet Pepsi, the new Christopher Cross tape blaring from the earphones of my Sony Walkman.'
+        'I’m wandering around VideoVisions, the video rental store near my apartment on the Upper West Side, sipping from a can of Diet Pepsi, the new Christopher Cross tape blaring from the earphones of my Sony Walkman.',
+        '“Listen. I’ll be daring,” Anne says finally. “I’ll have a Diet Coke with rum.”  Scott sighs, then smiles, beaming really. “Good.”  “That’s a caffeine-fine Diet Coke, right?” Anne asks the waiter.  “You know,” I interrupt, “you should have it with Diet Pepsi. It’s much better.”  “Really?” Anne asks. “What do you mean?”  “You should have the Diet Pepsi instead of the Diet Coke,” I say. “It’s much better. It’s fizzier. It has a cleaner taste. It mixes better with rum and has a lower sodium content.”'
+    ],
+    'perfume': [
+        '*The Patty Winters Show* this morning was about Perfumes and Lipsticks and Makeups.'
     ],
     'perrier': [
         'Before leaving my office for the meeting I take two Valium, wash them down with a Perrier and then use a scruffing cleanser on my face with premoistened cotton balls, afterwards applying a moisturizer.',
         'On the way back to my apartment I stop at D’Agostino’s, where for dinner I buy two large bottles of Perrier, a six-pack of Coke Classic, a head of arugula, five medium-sized kiwis, a bottle of tarragon balsamic vinegar, a tin of crême fraiche, a carton of microwave tapas, a box of tofu and a white-chocolate candy bar I pick up at the checkout counter.'
     ],
+    'rambo': [
+        '*The Patty Winters Show* this morning was about Real-Life Rambos.'
+    ],
     'scallop': [
         'Van Patten has the scallop sausage and the grilled salmon with raspberry vinegar and guacamole. The air-conditioning in the restaurant is on full blast and I\’m beginning to feel bad that I\’m not wearing the new Versace pullover I bought last week at Bergdorfs. It would look good with the suit I’m wearing.'
+    ],
+    'ufo': [
+        '*The Patty Winters Show* this morning was about UFOs That Kill.'
     ],
     'valium': [
         'Before leaving my office for the meeting I take two Valium, wash them down with a Perrier and then use a scruffing cleanser on my face with premoistened cotton balls, afterwards applying a moisturizer.'
